61c16aa17f70c553794185e4af3a00ec40f0cbd3
Anton <anton.lagosha.99@mail.ru>
2021-05-14 16:47:19 +0500

    + POST method /plus implemented

--- SimpleRequestHandler.cpp
+++ SimpleRequestHandler.cpp
@@ -1,6 +1,8 @@
 #include "SimpleRequestHandler.h"
 #include "SimpleEchoServer.h"
 
+#include <nlohmann/json.hpp>
+
 #include <Poco/URI.h>
 #include <Poco/StringTokenizer.h>
 
@@ -12,24 +14,32 @@ using namespace Poco::Net;
 void SimpleRequestHandler::handleRequest(Poco::Net::HTTPServerRequest& request,
                                          Poco::Net::HTTPServerResponse& response)
 {
-    response.setStatus(HTTPResponse::HTTP_OK);
-    response.setContentType("text/html");
-    std::ostream& out = response.send();
-
     URI uri(request.getURI());
-    if (uri.toString().find("/setText") == 0)
+    const auto requestStr = uri.toString();
+    if(requestStr.find("/plus") != 0)
     {
-        StringTokenizer str(uri.getQuery(), "=");
-        if (str.count() == 2 && str[0] == "text")
-        {
-            SimpleEchoServer::instance().setText(str[1]);
-            out << "ok";
-            out.flush();
-            return;
-        }
+        response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        return;
     }
 
-    out << "error";
+    std::istream& in = request.stream();
+    std::vector<char> requestBody(request.getContentLength(), '\0');
+    in.read(requestBody.data(), requestBody.size());
+
+    const auto jsonRequest = nlohmann::json::parse(requestBody);
+    const int a = jsonRequest["a"].get<int>();
+    const int b = jsonRequest["b"].get<int>();
+
+    nlohmann::json jsonResponse;
+    jsonResponse["a"] = a;
+    jsonResponse["b"] = b;
+    jsonResponse["sum"] = a + b;
+
+    response.setStatus(HTTPResponse::HTTP_OK);
+    response.setContentType("text/json");
+
+    std::ostream& out = response.send();
+    out << jsonResponse.dump(4);
     out.flush();
 }
 
